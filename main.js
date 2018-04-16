2cd05c69740f2440f532f9be320d07084c50fcbe
CashewRose <danieagnoletti@gmail.com>
2018-04-16 14:51:50 -0500

    added css file and formatted into a text convo page

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ const fragment = document.createDocumentFragment();
 const base = document.querySelector("#messages");
 
 const sec1 = document.createElement("section");
-sec1.textContent = "First section";
+sec1.textContent = "My parents are out of town tonight. Should I throw a wild part, or do what I normally do?";
 sec1.classList = "message";
 
 const sec2 = document.createElement("section");
-sec2.textContent = "Second section";
+sec2.textContent = "Which is...?";
 sec2.classList = "message";
 
 const sec3 = document.createElement("section");
-sec3.textContent = "Third section";
+sec3.textContent = "Dancing around the house in my underwear while singing peanut butter jelly time and smearing condiments all over my body.";
 sec3.classList = "message";
 
 const sec4 = document.createElement("section");
-sec4.textContent = "Fourth section";
+sec4.textContent = "No contest, shake that jelly!";
 sec4.classList = "message";
 
 const sec5 = document.createElement("section");
-sec5.textContent = "Fifth section";
+sec5.textContent = "I knew it!";
 sec5.classList = "message";
 
 fragment.appendChild(sec1);
